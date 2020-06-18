5e0bd6b84e8965ace4103532785db579aa85f5ff
karinelson52@gmail.com <karinelson52@gmail.com>
2020-06-18 17:36:56 -0400

    Final Save

---
+++ PlayingWithVariables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlayingWithVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Kari";
+            int age = 29;
+            char firstLetter = 'k';
+            bool isHappy; 
+            double temperature = 92.5;
+            decimal money = 150582.7m;
+
+            Console.WriteLine($"My name is { firstName }, I am {age} years old.");
+            Console.WriteLine($"My name starts with {firstLetter} and it is {temperature} outside today.");
+            Console.WriteLine($"I wish I had {money} in my bank account.");
+
+
+        }
+    }
+}
